160aacecd8a1485d87f4229b6cf4062e5b1e4834
Noone <zhu3521953@163.com>
2017-11-10 13:13:54 +0800

    add js to index.html

--- com.letao.www/public/backEnd/js/admin.js
+++ com.letao.www/public/backEnd/js/admin.js
@@ -1,6 +1,32 @@
-// 该文件的功能是用来写首页的js交互的
-//点击侧边栏隐藏
-$('.menu').on('click', function () {
-  $('.lt_aside').toggle()
-  $('.lt_main').toggleClass('move_right')
-})             
+$(function() {
+  // 该文件的功能是用来写首页的js交互的
+  //点击侧边栏隐藏
+  $(".menu").on("click", function() {
+    $(".lt_aside").toggle();
+    $(".lt_main").toggleClass("move_right");
+  });
+
+  //点击用户管理的效果
+  $('.manage-li').on('click', function () {
+    // $('.manage_button').removeClass('link')
+    $(this).siblings().children('a').removeClass('link')
+    $(this).children('a').addClass('link')
+    $('.breadcrumb').children().eq(2).hide()
+  })
+  //切换路径导航的显示
+  $('.manage_button').on('click', 'li', function () {
+    var $txt = $(this).children('a').text()
+    $('.breadcrumb').find('a').eq(1).html($txt)
+  })
+
+  $('#sort-two').on('click', 'li', function () {
+    var $txt = $(this).find('a').text()
+    // console.log(txt)
+    $('.breadcrumb').children().eq(2).show().html($txt)
+
+
+  })
+
+
+
+});
