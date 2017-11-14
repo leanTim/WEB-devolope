1b1037a7e9dfd42fb52bd3eaf9feded473717cc3
Noone <zhu3521953@163.com>
2017-11-14 23:11:34 +0800

    add some note

--- com.letao.www/public/frontEnd/js/detail.js
+++ com.letao.www/public/frontEnd/js/detail.js
@@ -1,5 +1,6 @@
 $(function () {
   var detailHTML = window.localStorage.getItem('detailData')
+  //因为页面是动态加载的 不能直接获取到id 要通过委托的方式获取id
   var productId = $('.slider-box').data('id')
   console.log(productId)
   $('.lt-detail').html(detailHTML)
@@ -9,7 +10,7 @@ $(function () {
       url: ' /cart/addCart',
       type: 'post',
       data: {
-        
+
       }
     })
 
